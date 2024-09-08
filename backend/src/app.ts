39614fe39dbdc31913203727a78a7b69205365ca
Quan Nguyen <mq003at@gmail.com>
2024-09-08 23:41:50 +0300

    move database into mongoDB Atlas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,6 +26,7 @@ mongoose.set('debug', true);
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(loggerMiddleware);
 app.use(
     session({
@@ -34,6 +35,7 @@ app.use(
         saveUninitialized: false
     })
 );
+
 // app.use(passport.initialize());
 // app.use(passport.session());
 
